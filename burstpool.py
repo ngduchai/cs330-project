b8ffbf53296262731c905ea9093c33a193e6859c
章超婕 <sherrychaojie@shouchous-MacBook-Pro.local>
2018-12-03 20:06:00 -0600

    burstpoo;

--- burstpool.py
+++ burstpool.py
@@ -1,15 +1,17 @@
 import rm
+import math
 
 class BurstPool(rm.Pool):
-    def __init__(self, weight, limit, inv_cost):
+    def __init__(self, weight, inv_cost):
         rm.Pool.__init__(self, weight)
-        self.runtime_limit = limit
+        self.runtime_limit = 16200
         self.inv_cost = inv_cost
         self.run_length = []
-        self.task_guarantee = 1#number of task
+        self.task_guarantee = 2#number of task
         self.max_resource = 5#number of maximum resource for each task
-        self.time_guarantee = 1#time frames for the guarantee
+        self.time_guarantee = 30#time frames for the guarantee
         self.requirement = self.task_guarantee*self.max_resource*(limit/self.time_guarantee)#minimum requirement for guarantees
+        self.counter = [0]*2
         # self.shrink_capacity = 0
 
     def extra_capacity(self):
@@ -17,7 +19,7 @@ class BurstPool(rm.Pool):
         if self.capacity <= self.requirement:
             return 0
         else:
-            unit_resource = max(self.run_length)/self.time_guarantee*self.max_resource*self.task_guarantee
+            unit_resource = math.ceil(max(self.run_length)/self.time_guarantee)*self.max_resource*self.task_guarantee
             return (self.capacity-self.requirement-max(self.capacity-self.free_capacity-unit_resource, 0))
 
     def reclaim(self, unit):
@@ -35,12 +37,13 @@ class BurstPool(rm.Pool):
     def launch_task(self, tasks):
         finished = []
         #add new tasks from input to the running_tasks list
-        max_launch = self.extra_capacity()+self.task_guarantee*self.max_resource
+        self.counter[0] += 1
+        max_launch = self.extra_capacity()+self.task_guarantee*self.max_resource-self.counter[1]
         for task in tasks:
             #launch if there's enough resource
-            if task.resource <= self.max_resource and task.resource <= max_launch and self.free_capacity >= task.resource:
+            if task.resource <= self.max_resource and task.resource <= max_launch and task.resource <= self.free_capacity:
                 self.free_capacity -= task.resource
-                max_launch -= task.resource
+                self.counter[1] += task.resource
                 task.status = rm.STATUS_RUNNING
                 self.running_tasks.append(task)
                 self.run_length.append(0)
@@ -70,6 +73,8 @@ class BurstPool(rm.Pool):
                 self.run_length.pop(i)
             else:
                 i += 1
+        if self.counter[0] >= 30:
+            self.counter = [0]*2
         self.reclaim(0)
         return finished
 
