a683f7fc7a74027131e726fae3bc98e293ef47c8
Lucy Newman <newmanlucy@uchicago.edu>
2018-12-03 23:00:04 -0600

    fixed free_capacity

--- ondemandpool.py
+++ ondemandpool.py
@@ -3,24 +3,24 @@ import rm
 class OnDemandPool(rm.Pool):
     def __init__(self, od_min_len, *args, **kwargs):
         super(OnDemandPool, self).__init__(*args, **kwargs)
-        # self.shrink_capacity = 0
-        self.shrink_left = 0
+        # self.free_capacity = 0
+        self.shrink_capacity = 0
         self.od_min_len = od_min_len
 
     def reclaim(self, unit):
         #reclaim resources
         #add unit to amount to be reclaimed shrink as much as possible
-        self.shrink_left += unit
-        if self.shrink_capacity >= self.shrink_left:
-            # shrink by all of shrink_left
-            self.shrink_capacity -= self.shrink_left
-            self.shrink_left = 0
+        self.shrink_capacity += unit
+        if self.free_capacity >= self.shrink_capacity:
+            # shrink by all of shrink_capacity
+            self.free_capacity -= self.shrink_capacity
+            self.shrink_capacity = 0
             return unit
         else:
-            # shrink by shrink_capacity
-            shrink_amt = self.shrink_capacity
-            self.shrink_left -= shrink_amt
-            self.shrink_capacity = 0
+            # shrink by free_capacity
+            shrink_amt = self.free_capacity
+            self.shrink_capacity -= shrink_amt
+            self.free_capacity = 0
             return shrink_amt
 
     def launch_task(self, tasks):
@@ -33,7 +33,6 @@ class OnDemandPool(rm.Pool):
             else:
                 task.status = rm.Status.REJECTED
                 finished.append(task)
-
         # run each task
         reclaimed = 0
         for task in self.running_tasks:
@@ -44,8 +43,8 @@ class OnDemandPool(rm.Pool):
                 finished.append(task)
        
         # reclaim additional resources when done running tasks
-        if self.shrink_left > 0:
-            self.shrink_capacity += reclaimed
+        if self.shrink_capacity > 0 and reclaimed > 0:
+            self.free_capacity += reclaimed
             self.reclaim(0)
         return finished
 
